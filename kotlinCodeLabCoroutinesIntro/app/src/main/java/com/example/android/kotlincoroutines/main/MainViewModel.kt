d418687ed57041a3cfebd06f75a2f9d9d5ee1e79
yingding <yingdingwang@yahoo.de>
2021-08-20 18:57:40 +0200

    replace submit with execute on ExecutorService class

--- kotlinCodeLabCoroutinesIntro/app/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
+++ kotlinCodeLabCoroutinesIntro/app/src/main/java/com/example/android/kotlincoroutines/main/MainViewModel.kt
@@ -103,7 +103,8 @@ class MainViewModel(private val repository: TitleRepository) : ViewModel() {
     private fun updateTaps() {
         // TODO: Convert updateTaps to use coroutines
         tapCount++
-        BACKGROUND.submit {
+        // BACKGROUND.submit {
+        BACKGROUND.execute{
             Thread.sleep(1_000)
             _taps.postValue("${tapCount} taps")
         }
