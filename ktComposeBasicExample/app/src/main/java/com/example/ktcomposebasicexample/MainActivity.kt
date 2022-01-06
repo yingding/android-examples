148e9f95ebed28992082bf89a9d7426af3fdb325
yingding <yingdingwang@yahoo.de>
2022-01-06 15:13:16 +0100

    updated compose modifier list

--- ktComposeBasicExample/app/src/main/java/com/example/ktcomposebasicexample/MainActivity.kt
+++ ktComposeBasicExample/app/src/main/java/com/example/ktcomposebasicexample/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.ktcomposebasicexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ktcomposebasicexample.ui.theme.KtComposeBasicExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +29,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    // Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true, name = "Text preview")
