158488e4c14fd4098c6b32efde99d666f13763ce
yingding <yingdingwang@yahoo.de>
2022-01-06 19:11:39 +0100

    updated multiple column and row structure

--- ktComposeBasicExample/app/src/main/java/com/example/ktcomposebasicexample/MainActivity.kt
+++ ktComposeBasicExample/app/src/main/java/com/example/ktcomposebasicexample/MainActivity.kt
@@ -3,8 +3,9 @@ package com.example.ktcomposebasicexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -18,30 +19,59 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             KtComposeBasicExampleTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    // Text(text = "Hello $name!")
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(
-            text = "Hello $name!",
-            modifier = Modifier.padding(24.dp)
-        )
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    // A surface container using the 'background' color from the theme
+//    Surface(color = MaterialTheme.colors.background) {
+//        Greeting("Android")
+//    }
+    val modifier = Modifier.padding(vertical = 4.dp)
+
+    Column (modifier = modifier){
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
-@Preview(showBackground = true, name = "Text preview")
+@Composable
+fun Greeting(name: String) {
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(
+            vertical = 4.dp,
+            horizontal = 8.dp
+        )
+
+    ) {
+        Row(Modifier.padding(24.dp)) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = {}
+            ) {
+                Text("Show more")
+            }
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    name = "Text preview",
+    widthDp = 320
+)
 @Composable
 fun DefaultPreview() {
     KtComposeBasicExampleTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
