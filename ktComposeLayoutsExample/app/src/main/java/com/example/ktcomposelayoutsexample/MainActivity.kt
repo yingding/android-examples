1909ba15d937db0f69aeff6eb99176feb7fb51f9
yingding <yingdingwang@yahoo.de>
2022-01-24 21:13:45 +0100

    updated PhotographerCard and its preview composable

--- ktComposeLayoutsExample/app/src/main/java/com/example/ktcomposelayoutsexample/MainActivity.kt
+++ ktComposeLayoutsExample/app/src/main/java/com/example/ktcomposelayoutsexample/MainActivity.kt
@@ -3,11 +3,22 @@ package com.example.ktcomposelayoutsexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ktcomposelayoutsexample.ui.theme.KtComposeLayoutsExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,10 +40,40 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun PhotographerCard() {
+    Row {
+        Surface (
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+
+        }
+        Column {
+            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+            // LocalContentAlpha is defining opacity level of its children
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text("3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     KtComposeLayoutsExampleTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PhotographerCardPreview() {
+    KtComposeLayoutsExampleTheme {
+        PhotographerCard()
     }
 }
