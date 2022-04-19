6199ac604f75edca54942d216b2f6b6d807685f3
yingding <yingdingwang@yahoo.de>
2022-04-19 17:37:17 +0200

    added navigation with argument to see single account

--- ktComposeNavigationExample/app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ ktComposeNavigationExample/app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -29,12 +29,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.compose.rally.data.UserData
 import com.example.compose.rally.ui.accounts.AccountsBody
+import com.example.compose.rally.ui.accounts.SingleAccountBody
 import com.example.compose.rally.ui.bills.BillsBody
 import com.example.compose.rally.ui.components.RallyTabRow
 import com.example.compose.rally.ui.overview.OverviewBody
@@ -93,16 +97,43 @@ fun RallyApp() {
                     // Text(text = RallyScreen.Overview.name)
                     OverviewBody(
                         onClickSeeAllAccounts = {navController.navigate(RallyScreen.Accounts.name)},
-                        onClickSeeAllBills = {navController.navigate(RallyScreen.Bills.name)}
+                        onClickSeeAllBills = {navController.navigate(RallyScreen.Bills.name)},
+                        onAccountClick = { name ->
+                            // trigger single account navigation
+                            navigateToSingleAccount(navController, name)
+                        }
                     )
                 }
                 composable(RallyScreen.Accounts.name) {
                     // Text(text = RallyScreen.Accounts.name)
-                    AccountsBody(accounts = UserData.accounts)
+                    AccountsBody(accounts = UserData.accounts) { name -> // onAccountClick
+                        // trigger single account navigation
+                        navigateToSingleAccount(
+                            navController = navController,
+                            accountName = name
+                        )
+                    }
                 }
                 composable(RallyScreen.Bills.name) {
                     // Text(text = RallyScreen.Bills.name)
                     BillsBody(bills = UserData.bills)
+                }
+                // set up the single account navi graph
+                var accountsName = RallyScreen.Accounts.name
+                composable(
+                    route = "$accountsName/{name}",
+                    arguments = listOf(
+                        navArgument("name") {
+                            // Make argument type safe
+                            type = NavType.StringType
+                        }
+                    )
+                ) { entry -> // Lock up "name" in NavBackStackEntry's arguments
+                    val accountName = entry.arguments?.getString("name")
+                    // Find first name match in UserData
+                    val account = UserData.getAccount(accountName)
+                    // Pass account to SingleAccountBody
+                    SingleAccountBody(account = account)
                 }
 
                 // TODO: Add the other two screen
@@ -115,4 +146,11 @@ fun RallyApp() {
             }
         }
     }
+}
+
+private fun navigateToSingleAccount(
+    navController: NavHostController,
+    accountName: String
+) {
+    navController.navigate("${RallyScreen.Accounts.name}/${accountName}")
 }
