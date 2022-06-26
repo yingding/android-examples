3d3130c11d438540a94158897873517080d068b0
yingding <yingdingwang@yahoo.de>
2022-06-26 22:01:31 +0200

    modified expresso test with hilt enabled using hilt testing lib

--- ktHiltExample/app/src/androidTest/java/com/example/android/hilt/AppTest.kt
+++ ktHiltExample/app/src/androidTest/java/com/example/android/hilt/AppTest.kt
@@ -26,19 +26,26 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import com.example.android.hilt.ui.MainActivity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
 import org.hamcrest.Matchers.containsString
 import org.junit.After
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class AppTest {
 
-    @After
-    fun tearDown() {
-        // Remove logs after the test finishes
-        ServiceLocator(getInstrumentation().targetContext).loggerLocalDataSource.removeLogs()
-    }
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+//    @After
+//    fun tearDown() {
+//        // Remove logs after the test finishes
+//        ServiceLocator(getInstrumentation().targetContext).loggerLocalDataSource.removeLogs()
+//    }
 
     @Test
     fun happyPath() {
