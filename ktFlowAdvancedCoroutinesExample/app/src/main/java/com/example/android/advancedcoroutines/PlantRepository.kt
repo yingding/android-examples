32d476e4feddef6867f49bfc6d82da487c59e20e
yingding <yingdingwang@yahoo.de>
2022-08-29 12:56:54 +0200

    updated view model with flow api

--- ktFlowAdvancedCoroutinesExample/app/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
+++ ktFlowAdvancedCoroutinesExample/app/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
@@ -32,6 +32,7 @@ import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.conflate
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.withContext
 
@@ -114,8 +115,18 @@ class PlantRepository private constructor(
                 }
             }
 
+/*    fun getPlantsWithGrowZoneFlow(growZone: GrowZone) : Flow<List<Plant>> {
+        return plantDao.getPlantsWithGrowZoneNumberFlow(growZone.number)
+    }*/
+
+    // added by me
     fun getPlantsWithGrowZoneFlow(growZone: GrowZone) : Flow<List<Plant>> {
         return plantDao.getPlantsWithGrowZoneNumberFlow(growZone.number)
+            .map { plantList ->
+                val sortOrderFromNetwork = plantsListSortOrderCache.getOrAwait()
+                val nextValue = plantList.applyMainSafeSort(sortOrderFromNetwork)
+                nextValue
+            }
     }
 
 
